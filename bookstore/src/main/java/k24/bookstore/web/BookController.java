5517259d7cbf79bac09dbf57b9f20804ba17657c
lottasep <seppala.lotta@gmail.com>
2024-02-06 14:25:18 +0200

    Add index page routing and index.html
    - Added GET mapping in BookController to handle requests to '/index'.
    - Created 'index.html' as a Thymeleaf page to display the index page.

--- bookstore/src/main/java/k24/bookstore/web/BookController.java
+++ bookstore/src/main/java/k24/bookstore/web/BookController.java
@@ -1,8 +1,14 @@
 package k24.bookstore.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BookController {
+
+    @GetMapping("/index")
+    public String showIndex() {
+        return "index";
+    }
 
 }
