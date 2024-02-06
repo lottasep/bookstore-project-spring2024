1aab320ba2236553dd3f4e7305a20315261e5209
lottasep <seppala.lotta@gmail.com>
2024-02-06 13:58:33 +0200

    Add attributes, constructors, getters/setters to Book.java

--- bookstore/src/main/java/k24/bookstore/domain/Book.java
+++ bookstore/src/main/java/k24/bookstore/domain/Book.java
@@ -1,5 +1,65 @@
 package k24.bookstore.domain;
 
+import java.math.BigDecimal;
+
 public class Book {
+    private String title;
+    private String author;
+    private int publicationYear;
+    private String isbn;
+    private BigDecimal price;
+
+    public Book() {
+    }
+
+    public Book(String title, String author, int publicationYear, String isbn, BigDecimal price) {
+        this.title = title;
+        this.author = author;
+        this.publicationYear = publicationYear;
+        this.isbn = isbn;
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    
 
 }
