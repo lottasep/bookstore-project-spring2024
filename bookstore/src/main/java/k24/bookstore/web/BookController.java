fe7b77088b328561254717b3f4d8c333229d416a
lottasep <seppala.lotta@gmail.com>
2024-02-08 11:38:45 +0200

    Implement add book functionality
    - Added 'addbook.html' Thymeleaf template for book addition form.
    - Added to BookController:
      - Logger for debugging and monitoring.
      - @GetMapping for '/addbook' to display the add book form.
      - @PostMapping for '/addbook' to handle form submissions to save a new book to the database.

--- bookstore/src/main/java/k24/bookstore/web/BookController.java
+++ bookstore/src/main/java/k24/bookstore/web/BookController.java
@@ -1,14 +1,21 @@
 package k24.bookstore.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import k24.bookstore.domain.Book;
 import k24.bookstore.domain.BookRepository;
 
 @Controller
 public class BookController {
+    private static final Logger log = LoggerFactory.getLogger(BookController.class);
+
     @Autowired
     private BookRepository repository;
 
@@ -21,6 +28,19 @@ public class BookController {
     public String showBooklist(Model model) {
         model.addAttribute("bookList", repository.findAll());
         return "booklist";
+    }
+
+    @GetMapping("/addbook")
+    public String addBook(Model model) {
+        log.info("Create a new book...");
+        model.addAttribute("book", new Book());
+        return "addbook";
+    }
+
+    @PostMapping("/addbook")
+    public String saveBook(@ModelAttribute ("book") Book book) {
+        repository.save(book);
+        return "redirect:booklist";
     }
 
 }
