f452d61103c4f63758e46d5d384fd2104bf5e35d
lottasep <seppala.lotta@gmail.com>
2024-02-11 22:54:14 +0200

    Update CommandLineRunner and Book toString()
    - Modified CommandLineRunner to save initial Category entities.
    - Updated test data in CommandLineRunner to associate Books with Categories.
    - Edited Book's toString() method to exclude category information to prevent the error from LazyInitializationException.

--- bookstore/src/main/java/k24/bookstore/BookstoreApplication.java
+++ bookstore/src/main/java/k24/bookstore/BookstoreApplication.java
@@ -11,6 +11,8 @@ import org.springframework.context.annotation.Bean;
 
 import k24.bookstore.domain.Book;
 import k24.bookstore.domain.BookRepository;
+import k24.bookstore.domain.Category;
+import k24.bookstore.domain.CategoryRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
@@ -21,11 +23,17 @@ public class BookstoreApplication {
 	}
 
     @Bean
-    public CommandLineRunner testData(BookRepository repository) {
+    public CommandLineRunner testData(BookRepository repository, CategoryRepository categoryRepository) {
         return (args) -> {
+            log.info("saving some categories");
+            Category sciFiCategory = categoryRepository.save(new Category("Science Fiction"));
+            Category computerCategory = categoryRepository.save(new Category("Computers & IT"));
+            Category childernsCategory = categoryRepository.save(new Category("Children's Books"));
+
             log.info("saving some books");
-            repository.save(new Book("Linna", "Franz Kafka", 2014, "9789529902262", BigDecimal.valueOf(23.90)));
-            repository.save(new Book("Python Crash Course", "Eric Matthes", 2023, "9781718502703", BigDecimal.valueOf(42.00)));
+            repository.save(new Book("Dune", "Frank Herbert", 2015, "9780340960196", BigDecimal.valueOf(11.50), sciFiCategory));
+            repository.save(new Book("Python Crash Course", "Eric Matthes", 2023, "9781718502703", BigDecimal.valueOf(42.00), computerCategory));
+            repository.save(new Book("The Very Hungry Caterpillar", "Eric Carle", 1994, "9780241003008", BigDecimal.valueOf(27.40), childernsCategory));
 
             log.info("get all books");
             for (Book book: repository.findAll()) {
--- bookstore/src/main/java/k24/bookstore/domain/Book.java
+++ bookstore/src/main/java/k24/bookstore/domain/Book.java
@@ -104,7 +104,7 @@ public class Book {
     @Override
     public String toString() {
         return "Book [id=" + id + ", title=" + title + ", author=" + author + ", publicationYear=" + publicationYear
-                + ", isbn=" + isbn + ", price=" + price + ", category=" + category + "]";
+                + ", isbn=" + isbn + ", price=" + price + "]";
     }
 
 }
