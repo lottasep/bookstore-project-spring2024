9988dbb937deb4eed0fed9df88f4ec08a58a0646
lottasep <seppala.lotta@gmail.com>
2024-02-11 20:46:39 +0200

    Add Category entity with one-to-many relationship to Book
    - Added new class Category with attributes id and name.
    - Added category attribute to Book class.
    - Added books attribute to Category class.
    - Included a 'books' list in Category.
    - Added getters and setters for the new attributes in Book and Category.
    - Added annotations for one-to many relationships.

--- bookstore/src/main/java/k24/bookstore/domain/Book.java
+++ bookstore/src/main/java/k24/bookstore/domain/Book.java
@@ -6,6 +6,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class Book {
@@ -19,6 +21,10 @@ public class Book {
     private String isbn;
     private BigDecimal price;
 
+    @ManyToOne
+    @JoinColumn(name="category_id")
+    private Category category;
+
     public Book() {
     }
 
@@ -28,6 +34,15 @@ public class Book {
         this.publicationYear = publicationYear;
         this.isbn = isbn;
         this.price = price;
+    }
+
+    public Book(String title, String author, int publicationYear, String isbn, BigDecimal price, Category category) {
+        this.title = title;
+        this.author = author;
+        this.publicationYear = publicationYear;
+        this.isbn = isbn;
+        this.price = price;
+        this.category = category;
     }
 
     public long getId() {
@@ -78,10 +93,18 @@ public class Book {
         this.price = price;
     }
 
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+    
     @Override
     public String toString() {
         return "Book [id=" + id + ", title=" + title + ", author=" + author + ", publicationYear=" + publicationYear
-                + ", isbn=" + isbn + ", price=" + price + "]";
+                + ", isbn=" + isbn + ", price=" + price + ", category=" + category + "]";
     }
 
 }
---
+++ bookstore/src/main/java/k24/bookstore/domain/Category.java
@@ -0,0 +1,62 @@
+package k24.bookstore.domain;
+
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private long id;
+    private String name;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
+    private List<Book> books;
+
+    public Category() {
+    }
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+
+    @Override
+    public String toString() {
+        return "Category [id=" + id + ", name=" + name + ", books=" + books + "]";
+    }
+
+    
+
+}
+
+
