0acb47c89dde81af4e8a5c5c3d0e120b432fa43d
lottasep <seppala.lotta@gmail.com>
2024-02-14 18:28:26 +0200

    Implement validation for book creation form
    - Add @Valid annotation and BindingResult in the saveBook method in BookController.
    - Modify addbook.html to display validation error messages for each form field including title, author, publication year, ISBN, and price.

--- bookstore/src/main/java/k24/bookstore/web/BookController.java
+++ bookstore/src/main/java/k24/bookstore/web/BookController.java
@@ -5,12 +5,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import jakarta.validation.Valid;
 import k24.bookstore.domain.Book;
 import k24.bookstore.domain.BookRepository;
 import k24.bookstore.domain.Category;
@@ -50,8 +52,14 @@ public class BookController {
         return "addbook";
     }
 
-    	@PostMapping("savebook")
-	public String saveBook(@ModelAttribute Book book) {
+    @PostMapping("savebook")
+	public String saveBook(@Valid @ModelAttribute("book") Book book, BindingResult result, Model model) {
+        log.info("CONTROLLER: Save the book - Check validation of book: " + book);
+        if (result.hasErrors()) {
+            model.addAttribute("categories", categoryRepository.findAll());
+            return "addbook";
+        }
+
 		repository.save(book);
 		return "redirect:/booklist";
 	}
