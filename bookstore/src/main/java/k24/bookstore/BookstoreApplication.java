8a45f7e1db00c9c2b15e0a0ffa3f30a5ed6e2abc
lottasep <seppala.lotta@gmail.com>
2024-03-16 18:59:09 +0200

    Add initial user and admin data loading via CommandLineRunner in BookstoreApplication

--- bookstore/src/main/java/k24/bookstore/BookstoreApplication.java
+++ bookstore/src/main/java/k24/bookstore/BookstoreApplication.java
@@ -9,6 +9,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import k24.bookstore.domain.AppUser;
+import k24.bookstore.domain.AppUserRepository;
 import k24.bookstore.domain.Book;
 import k24.bookstore.domain.BookRepository;
 import k24.bookstore.domain.Category;
@@ -23,7 +25,7 @@ public class BookstoreApplication {
 	}
 
     @Bean
-    public CommandLineRunner testData(BookRepository repository, CategoryRepository categoryRepository) {
+    public CommandLineRunner testData(BookRepository repository, CategoryRepository categoryRepository, AppUserRepository userRepository) {
         return (args) -> {
             log.info("saving some categories");
             Category sciFiCategory = categoryRepository.save(new Category("Science Fiction"));
@@ -35,6 +37,11 @@ public class BookstoreApplication {
             repository.save(new Book("Python Crash Course", "Eric Matthes", 2023, "9781718502703", BigDecimal.valueOf(42.00), computerCategory));
             repository.save(new Book("The Very Hungry Caterpillar", "Eric Carle", 1994, "9780241003008", BigDecimal.valueOf(27.40), childernsCategory));
 
+            AppUser user1 = new AppUser("user", "$2a$10$sV2YPS4yA/7XKnJBFnLr.ud9uTCbQfMAtiFSEk18BZj90tWIletJy", "USER");
+            AppUser user2 = new AppUser("admin", "$2a$10$MbnEy2V1tDaQ6k4zL8vUUuYIiXt8r5OP3ElbLQlF1Or3FfUSA93CW", "ADMIN");
+            userRepository.save(user1);
+            userRepository.save(user2);
+            
             log.info("get all books");
             for (Book book: repository.findAll()) {
                 log.info(book.toString());
