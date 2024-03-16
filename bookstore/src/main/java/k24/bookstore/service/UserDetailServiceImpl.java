371a4024bfcb15704bc4b41a7d8d15e93d3fbb42
lottasep <seppala.lotta@gmail.com>
2024-03-16 18:15:50 +0200

    Add UserDetailServiceImpl.java class

---
+++ bookstore/src/main/java/k24/bookstore/service/UserDetailServiceImpl.java
@@ -0,0 +1,28 @@
+package k24.bookstore.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import k24.bookstore.domain.AppUser;
+import k24.bookstore.domain.AppUserRepository;
+
+@Service
+public class UserDetailServiceImpl implements UserDetailsService  {
+	
+	@Autowired
+	AppUserRepository repository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException
+    {   
+    	AppUser curruser = repository.findByUsername(username);
+        UserDetails user = new org.springframework.security.core.userdetails.User(username, curruser.getPasswordHash(), 
+        		AuthorityUtils.createAuthorityList(curruser.getRole()));
+        return user;
+    }   
+} 
